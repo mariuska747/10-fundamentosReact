c2b18f5b0ed10f5860329bbdaccb58c666643ec9
enoc <keepcoding@mflapique.com>
2025-02-02 18:41:50 +0100

    clases

--- src/pages/adverts/AdvertsPage.tsx
+++ src/pages/adverts/AdvertsPage.tsx
@@ -1,3 +1,6 @@
+import styles from "./AdvertsPage.module.css"
+
+
 const adverts = [
   {
     id: "94d0ca7f-3a2c-4df8-8502-7c7273a58e67",
@@ -8,6 +11,18 @@ const adverts = [
     tags: ["lifestyle"],
     photo: "http://localhost:3001/public/1738247031129-909187199.jpeg",
   },
+  {
+    id: "57d7b36e-61eb-44b7-9432-c95f35a386b1",
+    createdAt: "2025-02-02T15:33:16.000Z",
+    name: "yamaha fazer",
+    sale: false,
+    price: 4000,
+    tags: [
+      "lifestyle",
+      "motor"
+    ],
+    photo: null
+  },
 ];
 
 function AdvertsPage() {
@@ -16,8 +31,8 @@ function AdvertsPage() {
       <h1>Adverts</h1>
         <ul>
             {adverts.map((advert) => (
-            <li key={advert.id}>
-                <img src={advert.photo} alt={advert.name} width="50px"/>
+            <li key={advert.id} className={styles.advert}>
+                <img src={advert.photo ? advert.photo : ""} alt={advert.name} width="50px"/>
                 <h3>{advert.name}</h3>
                 <p>Precio: {advert.price} €</p>
                 <p>Compra/Venta: {advert.sale ? "Sale" : "Purchase"}</p>
